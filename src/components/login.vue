eff7e1652baa090599c327d41729f1d468705472
YJ <18298405849@163.com>
2017-11-29 10:53:13 +0800

    add axios to my-login

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,59 +1,49 @@
 <template>
   <!--模板-->
- <!-- <h1>
-    {{ message }}
-  </h1>-->
-
   <form novalidate>
-    <label for="username">Username</label>
-    <input type="text" placeholder="Please Enter your Username" id="username" v-model="username">
+    <label for="username">用户名</label>
+    <input type="text" placeholder="Please enter your username" id="username" v-model="username">
     <br>
-    <label for="password">Password</label>
-    <input type="text" placeholder="Please Enter your Password" id="password" v-model="password">
+    <label for="password">密码</label>
+    <input type="password" placeholder="Please enter your password" id="password" v-model="password">
     <br>
-    <button type="button" v-on:click="login">Login</button>
+    <button v-on:click="login" type="button">Login</button>
   </form>
-
 </template>
 
 <script>
-  /*JS*/
+  /*js*/
   export default {
     name: 'login',
     data: function () {
       return {
-       /* message: 'Hello YJ,Your Very Good!',*/
         username: '',
         password: ''
       }
     },
     methods: {
-        login: function () {
-         /* axios.post('http://127.0.0.1:5000/login', {
-            username: this.username,
-            password: this.password
-          }).then(function (response) {
-            console.log(response)
-          }).catch(function (error) {
-            console.log(error.response)
-          })*/
-          var request = new XMLHttpRequest();
-          request.open("POST", 'http://127.0.0.1:5000/login');
-          request.onreadystatechange = function(){
-            if(request.readyState === 4 && request.status == 200){
-              console.log(request);
-            }
-          };
-          request.setRequestHeader("Content-Type","application/json");
-          request.send(JSON.stringify({"username": this.username,"password": this.password}));
-        }
+      login: function () {
+        this.$http.post('http://127.0.0.1:5000/login', {
+          username: this.username,
+          password: this.password
+        }).then(function (response) {
+          console.log(response);
+          if(parseInt(response.code) === 400){
+            // 登录失败
+
+
+          }else if (parseInt(response.code) === 200){
+            // 登录成功
+
+          }
+        }).catch(function (error) {
+          console.log(error)
+        })
+      }
     }
   }
-
 </script>
 
 <style>
   /*样式*/
-
-
 </style>
